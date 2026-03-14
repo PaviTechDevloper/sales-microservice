73827f3e5ff54596c69cb308a97da35b3d296ed7
Shubha-KA <307415@ust.com>
2026-03-14 15:24:49 +0530

    Added order service

--- sales-order-service/app/init_db.py
+++
@@ -1,9 +0,0 @@
-from app.database import engine, Base
-from app.models.order import Order
-from app.models.order_item import OrderItem
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    create_tables()
--- sales-order-service/app/utils/service_client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def authenticated_get(url, token):
-
-    return requests.get(
-        url,
-        headers={"Authorization": token},
-        timeout=5
-    )
